73a4c2a77d558ec09a2ac02002b7742c3ce6fdd5
rsh23 <rodrigo.salinas23@hotmail.com>
2022-08-18 19:21:45 -0400

    Apuntes de los constructores con nombre en dart

---
+++ Dart/constructores con nombre.dart
@@ -0,0 +1,51 @@
+void main (){
+  
+  final rawJson = {
+    
+    'vida' : 100,
+    'nombre' : 'Tony',
+    'poder' : 'Dinero'
+    
+  };
+  
+  final ironman = Heroe.fromJson( rawJson );
+  
+  print( ironman );
+  
+//   final wolverine = new Heroe( nombre: 'Logan', vida: 200, poder: 'Regeneracion' );
+  
+
+//   print( wolverine );
+  
+}
+
+
+class Heroe{
+  
+//   Parametros o propiedades de la clase Heroe
+  int vida;
+  String nombre;
+  String poder;
+  
+//   Esto es un constructor de la clase Hereo, en donde inicializamos las variables
+  Heroe({ required this.vida, required this.nombre, required this.poder });
+  
+//   Esta es la syntaxis de un constructor con nombre
+  Heroe.fromJson( Map<String, dynamic> json ):
+    
+//   El '!' es para que decirle a dart que si o si recibira el valor, por lo tanto no arroja error.
+    this.vida = json['vida']!,  
+    this.nombre = json['nombre'] ?? 'No tiene nombre',
+    this.poder = json['poder'] ?? 'No tiene poder';
+    
+//     El '??' es en caso de que el parametro sea null
+    
+  
+  
+//   Esto sobrescribe el metodo "toString"
+  @override
+  String toString(){
+    return 'Vida: ${this.vida}, Nombre: ${this.nombre}, Poder: ${this.poder}';
+  }
+  
+}
